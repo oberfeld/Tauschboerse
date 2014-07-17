87aecec43bd0134dc43ae73446af4bf9bc8f7bff
Markus124 <markus.klaesle@gmail.com>
2014-07-17 18:24:10 +0200

    fixes #4 Link to 'Startseite' at button of page

--- application/views/book/success.php
+++ application/views/book/success.php
@@ -1,1 +1,15 @@
-Buch erfolgreich in die Datenbank eingetragen
+<?php $this->load->helper('url');?>
+
+<html>
+<head>
+<title>Success!</title>
+</head>
+<body>
+
+<h3>Buch erfolgreich in die Datenbank eingetragen</h3>
+
+<p><?php echo anchor('book/create', 'Weitere Bücher eintragen'); ?></p>
+<p><?php echo anchor('', 'Startseite'); ?></p>
+
+</body>
+</html>
