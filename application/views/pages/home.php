cf5cde55ef4156a4b3f5d953c71be959fec2c176
Markus124 <markus.klaesle@gmail.com>
2014-09-23 21:17:27 +0200

    Links on Homepage as buttons

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,11 +5,15 @@ $this->load->helper('url');
 
 echo '
 <h2>Funktion auswählen</h2>
-<p>'  . anchor('book/create','Bücher eintragen') . ' </p>
-<p>'  . anchor('book/booklist','Alle Bücher anzeigen') . ' </p>
-<p> ' . anchor('book/booklist','Alle Bücher anzeigen', 'class="btn btn-default" role="button"') . ' </p>
+<p>
+	'.anchor('book/create','Bücher eintragen', 'class="btn btn-default" role="button"').'
+</p>
+<p>
+	'.anchor('book/booklist','Alle Bücher anzeigen', 'class="btn btn-default" role="button"').'
+</p>
 
 <p><b><em>ToDo:</em></b> Auswahl weiterer Funktionen</p>';
 
 		
-include APPPATH . 'views/templates/footer.php';?>
+include APPPATH . 'views/templates/footer.php';
+?>
