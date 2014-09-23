2044e63ef7f193ddd2121b842f4f122fa2c30002
Markus124 <markus.klaesle@gmail.com>
2014-09-23 21:18:53 +0200

    Not working yet! Remove-buttons in view booklist as buttons with confirmation

--- application/views/book/booklist.php
+++ application/views/book/booklist.php
@@ -15,8 +15,9 @@ echo '
     <th>Löschen</th>
   </tr>';
 
-
+$i = 1;
 foreach ($book->result() as $book_item) {
+	$i += 1;
 	echo'
   <tr>
 	<td class="hidden">' . $book_item->id . '</td>		
@@ -24,7 +25,27 @@ foreach ($book->result() as $book_item) {
 	<td>' . $book_item->title . '</td>
 	<td>' . anchor('book/details/' . $book_item->id, 'Details'). '</td>
 	<td>' . anchor('book/edit/' . $book_item->id, 'Bearbeiten'). '</td>
-	<td>' . anchor('book/remove/' . $book_item->id, 'Löschen'). '</td>
+	<td><button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#ConfirmRemove'.$i.'">
+			Löschen
+		</button>
+		<div class="modal fade" id="ConfirmRemove'.$i.'" tabindex="-1" role="dialog" aria-labelledby="ConfirmRemoveLabel'.$i.'" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Abbrechen</span></button>
+						<h4 class="modal-title">Bestätigung</h4>
+					</div>
+					<div class="modal-body">
+						<p>Soll das Buch '.$book_item->title.' von '.$book_item->author.' wirklich gelöscht werden?</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+						'.anchor('book/remove/' . $book_item->id,'Buch löschen', 'class="btn btn-primary" type="button"').'
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
+	</td>
   </tr>';
 }
 
@@ -33,5 +54,8 @@ echo
 '</table>
 
 <p>' . anchor('', 'Startseite') . '</p>';
+
+
+
 
 include APPPATH . 'views/templates/footer.php';
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -24,6 +24,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    <script src="<?php echo base_url();?>dist/js/bootstrap.min.js"></script>
   </head>
 
   <body>
@@ -41,7 +43,7 @@
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="<?= base_url();?>index.php/">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
